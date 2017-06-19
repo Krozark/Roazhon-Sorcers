dd3849e4c5e1dd31534a3b4024a8a9ed0f97ab5b
Krozark <maxime.barbier1991@gmail.com>
2017-06-19 20:04:14 +0200

    initiaj commit

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#from django.conf import settings
+from django.conf.urls import include,url
+#from website.views import TestView
+#from website.models import *
+
+
+urlpatterns = [
+]
