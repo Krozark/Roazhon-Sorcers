ccce3e0eb5a24ca4017885bc5ef65f3f549557e3
Krozark <maxime.barbier1991@gmail.com>
2017-07-07 19:03:40 +0200

    split settings files

---
+++ roazhon_sorcers/settings/dev.py
@@ -0,0 +1,27 @@
+import os
+
+from roazhon_sorcers.settings.base import *
+
+SECRET_KEY = '6)9!5x!bmk9s_5%8*tn$2uin0i6v2y34b7!ainw$yd^wfd_6$&'
+
+
+DEV = True
+DEBUG = DEV
+DEBUG_PROPAGATE_EXCEPTIONS = DEBUG
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+EMAIL_HOST = "smtp.alwaysdata.com"
+EMAIL_PORT = 25
--- roazhon_sorcers/wsgi.py
+++ roazhon_sorcers/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roazhon_sorcers.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roazhon_sorcers.settings.dev")
 
 application = get_wsgi_application()
