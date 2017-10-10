f3c9bc686773ea47254876fc7bf2f238b0f122e7
Maxime Barbier <maxime.barbier1991@gmail.com>
2017-10-10 23:00:16 +0200

    remove mendatory image

--- website/models.py
+++ website/models.py
@@ -35,7 +35,7 @@ class Article(models.Model, HitCountMixin):
     creation_date               = models.DateTimeField(_('Creation date'), default=datetime.now())
     publishing_date    = models.DateTimeField(_('Publication date'), default=datetime.now(), help_text= _("This date specify the minimal date require to see the article"))
     created_by         = models.ForeignKey(User)
-    image              = ResizedImageField(upload_to='uploads/article',blank=False, size=[1920, 1080], crop=['middle', 'center'], quality=75)
+    image              = ResizedImageField(upload_to='uploads/article',blank=True, size=[1920, 1080], crop=['middle', 'center'], quality=75)
     M2M_category       = models.ManyToManyField(ArticleCategory)
     draft              = models.BooleanField(_("Draft"), default=True, help_text=_("The post will no apear in the website in draft state"))
     content            = FroalaField(help_text=_("You can use http://www.strawpoll.me/ to create polls"))
